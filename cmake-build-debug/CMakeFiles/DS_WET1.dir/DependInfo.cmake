159970c82ab1fe79e2305b3fc22f614c06c921b8
Saleh Hassan <salihhassan@campus.technion.ac.il>
2022-04-23 17:03:42 +0300

    updated

---
+++ cmake-build-debug/CMakeFiles/DS_WET1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Saleh/Desktop/Data-Structures/spring/DS-WET1/Company.cpp" "/mnt/c/Users/Saleh/Desktop/Data-Structures/spring/DS-WET1/cmake-build-debug/CMakeFiles/DS_WET1.dir/Company.cpp.o"
+  "/mnt/c/Users/Saleh/Desktop/Data-Structures/spring/DS-WET1/Employee.cpp" "/mnt/c/Users/Saleh/Desktop/Data-Structures/spring/DS-WET1/cmake-build-debug/CMakeFiles/DS_WET1.dir/Employee.cpp.o"
+  "/mnt/c/Users/Saleh/Desktop/Data-Structures/spring/DS-WET1/StockExchange.cpp" "/mnt/c/Users/Saleh/Desktop/Data-Structures/spring/DS-WET1/cmake-build-debug/CMakeFiles/DS_WET1.dir/StockExchange.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/DS_WET1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DS_WET1.dir/Company.cpp.o"
+  "CMakeFiles/DS_WET1.dir/Employee.cpp.o"
+  "CMakeFiles/DS_WET1.dir/StockExchange.cpp.o"
+  "DS_WET1"
+  "DS_WET1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DS_WET1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
