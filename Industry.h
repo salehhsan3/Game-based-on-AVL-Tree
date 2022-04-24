ea85493e87527eb5c8ae678c8bb3f0e6d5bd3399
Saleh Hassan <salihhassan@campus.technion.ac.il>
2022-04-24 23:02:17 +0300

    industry

--- Industry.h
+++ Industry.h
@@ -34,6 +34,40 @@ namespace MIVNI{
         Industry& operator=(const Industry& ind) = default;
         ~Industry()= default;
         ~Industry();
+
+
+        Industry *Init();
+
+        StatusType AddCompany(int CompanyID, int Value);
+
+        StatusType AddEmployee(int EmployeeID, int CompanyID, int Salary, int Grade);
+
+        StatusType RemoveCompany(int CompanyID);
+
+        StatusType RemoveEmployee(int EmployeeID);
+
+        StatusType GetCompanyInfo(int CompanyID, int *Value, int *NumEmployees);
+
+        StatusType GetEmployeeInfo(int EmployeeID, int *EmployerID, int *Salary, int *Grade);
+
+        StatusType IncreaseCompanyValue(int CompanyID, int ValueIncrease);
+
+        StatusType PromoteEmployee(int EmployeeID, int SalaryIncrease, int BumpGrade);
+
+        StatusType HireEmployee(int EmployeeID, int NewCompanyID);
+
+        StatusType AcquireCompany(int AcquirerID, int TargetID, double Factor);
+
+        StatusType GetHighestEarner(int CompanyID, int *EmployeeID);
+
+        StatusType GetAllEmployeesBySalary(int CompanyID, int **Employees, int *NumOfEmployees);
+
+        StatusType GetHighestEarnerInEachCompany(int NumOfCompanies, int **Employees);
+
+        StatusType GetNumEmployeesMatching(int CompanyID, int MinEmployeeID, int MaxEmployeeId,
+                int MinSalary, int MinGrade, int *TotalNumOfEmployees, int *NumOfEmployees);
+
+
     };
 
 }
